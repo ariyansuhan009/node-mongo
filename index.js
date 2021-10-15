8872da55dbf79dec92192591e3a4fb95a5dfdb4d
ariyansuhan009 <ariyansuhan009@gmail.com>
2021-10-15 14:49:38 +0000

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+const { application } = require('express');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const users = ['Ariyan', 'Sohan', 'Sumon', 'Emon', 'Ismail', 'Dulal'];
+
+app.get('/', (req, res) => {
+     const fruit = {
+          product: 'ada',
+          price: 220
+     }
+
+     res.send(fruit);
+});
+app.get('/fruits/banana', (req, res) => {
+     res.send({fruit: 'banana', quantity: 1000, price: 10000 });
+});
+
+app.get('/users/:id', (req, res) => {
+     const id = req.params.id;
+     console.log(req.query);
+     const name = users[id];
+     res.send({id, name});
+})
+   
+app.listen(3000, () => console.log("Lisienting to port 3000"))
